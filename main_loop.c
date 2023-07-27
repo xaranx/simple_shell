2ada118046a2e822bd7d39fe79c3360f65fe1f2e
koldflames <enoeihoda@gmail.com>
2023-07-27 06:54:39 -0700

    updated main loop again

--- main_loop.c
+++ main_loop.c
@@ -6,37 +6,49 @@
  *
  * Return: no return.
  */
+
+
+/* Shell Loop Function */
 void shell_loop(data_shell *datash)
 {
 	int loop, i_eof;
 	char *input;
 
-	loop = 1;
-	while (loop == 1)
+	loop = 1; /* Set the loop control variable to 1 to start the loop */
+	while (loop == 1) /* Execute the loop while the loop control variable is 1 */
 	{
+		/* Print the shell prompt */
 		write(STDIN_FILENO, "$ ", 2);
+
+		/* Read the input line, and check for end-of-file (i_eof) condition */
 		input = read_line(&i_eof);
-		if (i_eof != -1)
+
+		if (i_eof != -1) /* If it's not an end-of-file condition */
 		{
+			/* Remove comments from the input */
 			input = without_comment(input);
 			if (input == NULL)
-				continue;
+				continue; /* Skip to the next iteration if the input is empty */
 
+			/* Check for syntax errors in the input */
 			if (check_syntax_error(datash, input) == 1)
 			{
-				datash->status = 2;
-				free(input);
-				continue;
+				datash->status = 2; /* Set the status to 2 for syntax error */
+				free(input); /* Free the allocated memory for input */
+				continue; /* Skip to the next iteration of the loop */
 			}
+			/* Replace variables in the input */
 			input = rep_var(input, datash);
+			/* Split and execute commands from the input */
 			loop = split_commands(datash, input);
-			datash->counter += 1;
-			free(input);
+
+			datash->counter += 1; /* Increment the command counter */
+			free(input); /* Free the allocated memory for input */
 		}
 		else
 		{
-			loop = 0;
-			free(input);
+			loop = 0; /* Set loop control variable to 0 to exit loop */
+			free(input); /* Free allocated memory for input */
 		}
 	}
 }
